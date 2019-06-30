d5eadaf4cb764e6bb2fcc2212c2b16838f8db111
Gabreil Freire <gabriellsf@gmail.com>
2019-06-30 11:17:34 -0300

    Mock de Motoristas

--- hackaton-uber-backend/app.py
+++ hackaton-uber-backend/app.py
@@ -28,11 +28,6 @@ db = client.uberhack
 def hello():
     return  "Bem vindo a API MeuFavorito"
 
-#API para Listagem
-@app.route('/listar/<nome>/<empresa>')
-def buscarTodos(nome, empresa):
-    return  jsonify(resultado=db[nome].find({"empresa": empresa}))
-
 #Endpoint GET para buscar favoritos de um cliente 
 #Endpoint POST para salvar favorito entre cliente e motorista
 @app.route('/favorites', methods=['GET','POST'])
@@ -134,53 +129,45 @@ def agendar():
 ## At 30 de Julho
 @app.route('/riders')
 def geraViagensRecentes():
-    return {
+    return jsonify(resultado={"drivers": [
+    {
         "id": "1",
         "name": "Carlos Abreu",
-        "photo": 6.17,
+        "photo": "https://randomuser.me/api/portraits/men/25.jpg"
     },
     {
         "id": "2",
         "name": "Roberto Ricardo",
-        "photo": 6.17,
+        "photo": "https://randomuser.me/api/portraits/men/86.jpg",
     },
     {
         "id": "3",
         "name": "Giovanna Luz",
-        "photo": 6.17,
+        "photo": "https://randomuser.me/api/portraits/women/82.jpg",
     },
     {
         "id": "4",
         "name": "Roberta Campos",
-        "photo": 6.17,
+        "photo": "https://randomuser.me/api/portraits/women/95.jpg",
     },
     {
         "id": "5",
         "name": "Marcela Santos",
-        "photo": 6.17,
+        "photo": "https://randomuser.me/api/portraits/women/41.jpg",
     },
     {
         "id": "6",
         "name": "Luana Freire",
-        "photo": 6.17,
+        "photo": "https://randomuser.me/api/portraits/women/62.jpg",
     }
+  ]
+  })
 
 ## Mock da API da uber pois a mesma está com problemas de como confirmado pelo Engenheiro responsavel no evento
 ## At 30 de Julho
 @app.route('/estimate/price')
 def geraEstimativa():
     return jsonify(resultado={"prices": [
-    {
-      "localized_display_name": "POOL",
-      "distance": 6.17,
-      "display_name": "POOL",
-      "product_id": "26546650-e557-4a7b-86e7-6a3942445247",
-      "high_estimate": 15,
-      "low_estimate": 13,
-      "duration": 1080,
-      "estimate": "$13-14",
-      "currency_code": "USD"
-    },
     {
       "localized_display_name": "uberX",
       "distance": 6.17,
@@ -190,72 +177,6 @@ def geraEstimativa():
       "low_estimate": 13,
       "duration": 1080,
       "estimate": "$13-17",
-      "currency_code": "USD"
-    },
-    {
-      "localized_display_name": "uberXL",
-      "distance": 6.17,
-      "display_name": "uberXL",
-      "product_id": "821415d8-3bd5-4e27-9604-194e4359a449",
-      "high_estimate": 26,
-      "low_estimate": 20,
-      "duration": 1080,
-      "estimate": "$20-26",
-      "currency_code": "USD"
-    },
-    {
-      "localized_display_name": "SELECT",
-      "distance": 6.17,
-      "display_name": "SELECT",
-      "product_id": "57c0ff4e-1493-4ef9-a4df-6b961525cf92",
-      "high_estimate": 38,
-      "low_estimate": 30,
-      "duration": 1080,
-      "estimate": "$30-38",
-      "currency_code": "USD"
-    },
-    {
-      "localized_display_name": "BLACK",
-      "distance": 6.17,
-      "display_name": "BLACK",
-      "product_id": "d4abaae7-f4d6-4152-91cc-77523e8165a4",
-      "high_estimate": 43,
-      "low_estimate": 43,
-      "duration": 1080,
-      "estimate": "$43.10",
-      "currency_code": "USD"
-    },
-    {
-      "localized_display_name": "SUV",
-      "distance": 6.17,
-      "display_name": "SUV",
-      "product_id": "8920cb5e-51a4-4fa4-acdf-dd86c5e18ae0",
-      "high_estimate": 63,
-      "low_estimate": 50,
-      "duration": 1080,
-      "estimate": "$50-63",
-      "currency_code": "USD"
-    },
-    {
-      "localized_display_name": "ASSIST",
-      "distance": 6.17,
-      "display_name": "ASSIST",
-      "product_id": "ff5ed8fe-6585-4803-be13-3ca541235de3",
-      "high_estimate": 17,
-      "low_estimate": 13,
-      "duration": 1080,
-      "estimate": "$13-17",
-      "currency_code": "USD"
-    },
-    {
-      "localized_display_name": "WAV",
-      "distance": 6.17,
-      "display_name": "WAV",
-      "product_id": "2832a1f5-cfc0-48bb-ab76-7ea7a62060e7",
-      "high_estimate": 33,
-      "low_estimate": 25,
-      "duration": 1080,
-      "estimate": "$25-33",
       "currency_code": "USD"
     },
     {
